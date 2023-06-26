60b2b3229d4c40804089b4eaa8bfc03467cf607d
SREERAG M <sreeragm030@gmail.com>
2023-06-26 23:14:58 +0530

    login and signup completed

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,6 +1,7 @@
 import React, {  useState } from "react";
 import { Link, useNavigate } from 'react-router-dom';
 import Axios from 'axios';
+import myImage from 'D:/coding/dbms project/project-s4-frontend/src/mymess_logo.png';
 const Login = () => {
     const [StudentID, setStudentID] = useState('')
     const [Password, setPassword] = useState('')
@@ -28,16 +29,22 @@ const navigate = useNavigate()
     
     return (
         <div className="InputDiv">
-
-            <h1>Login</h1>
+            <div className="loginPart">
+            <h1 id="loginHead">Login</h1>
             <input className="InputBox" type="text" value={StudentID} onChange={(e) => setStudentID(e.target.value)} placeholder="Enter Student ID" />
             <input className="InputBox" type="password" value={Password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter password" />
             <button type="button" onClick={CollectData}>LOGIN</button>
             <div className="Link-signup">
                 Dont have an account? click here to &nbsp;
-                <Link to={'/signup'}>
+                <Link id = "signupid" to={'/signup'}>
                     REGISTER
                 </Link>
+            </div>
+            </div>
+            <div className="logoPart">
+            <img id = "logo" src={myImage} alt="logo" />
+            <h1 id="mymess">MyMess</h1>
+                
             </div>
         </div>
     )
