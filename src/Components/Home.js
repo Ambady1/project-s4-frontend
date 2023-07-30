770da7584809308bbd36f7b5457f6f34649a481b
Ambady <cssreedeep@gmail.com>
2023-07-30 20:04:49 +0530

    Notification updation
    Notification from backend is applied - Changes in CSS needed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,8 +9,11 @@ import payBill from '../assets/payBill.png';
 import feedback from '../assets/feedback.png';
 import messMenu from '../assets/messMenu.png';
 import rules from '../assets/rules.png';
-
+import axios from 'axios';
+import { useState,useEffect } from 'react';
 const Home = ({auth,loginStatus}) => {
+
+  const [newsItems, setNewsItems] = useState([]);
 
   const navigate = useNavigate();
   const handleProfile=()=> {
@@ -32,6 +35,19 @@ const Home = ({auth,loginStatus}) => {
     navigate('/livemessbill')
   }
   
+   //Useeffect for news 
+   useEffect(() => {
+    axios.get("http://localhost:8800/news")
+        .then(response => {
+            setNewsItems(response.data);
+        })
+        .catch(error => {
+            console.log("Error occurred while fetching news(frontend):", error);
+        });
+
+}, [])
+
+console.log(newsItems)
   return (
     <div className='homeTotalContainer'>
       {auth ? (
@@ -39,21 +55,17 @@ const Home = ({auth,loginStatus}) => {
         <div className='homeContainer'>
         <h1 className='welcomeText' id='welcomeId'>Welcome, {loginStatus.name}!</h1>
 
-        <div class="notifSlider">
-          <div class="ticker">
-              <div class="notifLabel">NOTIFICATIONS</div>
-              <span class="notifBreaker"></span>
-              <div class="notifText">Siberia Hostel Mess started on 4th July, 2023...</div>
-              <span class="notifBreaker"></span>
-              <div class="notifText">Sagar Hostel Mess started on 7th July, 2023...</div>
-              <span class="notifBreaker"></span>
-              <div class="notifText">Swaraj Hostel Mess started on 10th July, 2023...</div>
-              <span class="notifBreaker"></span>
-              <div class="notifText">Sahara Hostel Mess admission started on 10th July, 2023...</div>
-              <span class="notifBreaker"></span>
-              <div class="notifText">Home delivery available now in Sagar Hostel Mess at Rs 120/- per day...</div>
-          </div>
-        </div>
+        <div className="notifSlider">
+              <div className="ticker">
+                <div className="notifLabel">NOTIFICATIONS</div>
+                {newsItems.map((item) => (
+                  <React.Fragment key={item.id}>
+                    <span className="notifBreaker"></span>
+                    <div className="notifText">{item.message}</div>
+                  </React.Fragment>
+                ))}
+              </div>
+            </div>
 
         <div className="card-container">
             <div className="card" id="viewProfile" onClick={handleProfile}>
